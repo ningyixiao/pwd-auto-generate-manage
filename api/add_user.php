a58dd748d76b73def0333f86001fa2937588a594
ningyixiao <zjutsgg@gmail.com>
2017-05-22 23:47:09 +0800

    complete the sql file

--- api/add_user.php
+++ api/add_user.php
@@ -3,7 +3,7 @@
     require('init_0.php');
     @$nickname=$_REQUEST['nickname'];
     @$pwd=$_REQUEST['pwd'];
-    $sql="INSERT INTO user (uid,nickname,pwd,masterKey) VALUES (NULL,'$nickname','$pwd','');";
+    $sql="INSERT INTO User (uid,nickname,pwd,masterKey) VALUES (NULL,'$nickname','$pwd','');";
     $result=mysqli_query($conn,$sql);
     if($result){
         $data['msg']='add user success';
--- res/sql/keygen.sql
+++ res/sql/keygen.sql
@@ -3,11 +3,29 @@ DROP DATABASE IF EXISTS keygen;
 CREATE DATABASE keygen CHARSET=UTF8;
 USE keygen;
 
-CREATE TABLE user(
+CREATE TABLE User(
     uid INT PRIMARY KEY AUTO_INCREMENT,
     nickname VARCHAR(64),
     pwd VARCHAR(256),
-    masterKey VARCHAR(256)
+    openKey VARCHAR(256)
+);
+CREATE TABLE Group(
+    gid INT PRIMARY KEY AUTO_INCREMENT,
+    groupName VARCHAR(64),
+    owner VARCHAR(256),
+    member VARCHAR(256),
+    openKey VARCHAR(256)
+);
+CREATE TABLE Key(
+    kid INT PRIMARY KEY AUTO_INCREMENT,
+    owner VARCHAR(256),
+    feature VARCHAR(256),
+    genKey VARCHAR(256),
+    shareState INT,
+    uKey VARCHAR(256),
+    gKey VARCHAR(256),
+    targetUser VARCHAR(64),
+    targetGroup VARCHAR(64)
 );
 -- INSERT INTO user(uid,nickname,pwd,masterKey) VALUES
 -- (   null,
